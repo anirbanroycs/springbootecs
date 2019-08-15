053d413e0f5f3e83ba8262cb6546ba36178cc5f0
anirbanroycs <anirban.roy.cs@gmail.com>
2019-08-15 17:22:05 +0530

    Update Demo2Application.java

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -2,8 +2,7 @@ package com.example.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 public class Demo2Application {
